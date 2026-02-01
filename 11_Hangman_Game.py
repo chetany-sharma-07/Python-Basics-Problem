b34d6369ed8ca2c6a32a9cef4cea399a09586bdf
chetany-sharma-07 <sharmachetany1004@gmail.com>
2026-02-02 02:18:05 +0530

    Make the program to guess again and update display as per new guesses and checking player has won.

--- 11_Hangman_Game.py
+++ 11_Hangman_Game.py
@@ -10,12 +10,25 @@ for position in range(len(choosen_word)):
     placeholder+="_"
 print(placeholder)
 
-guess_letter=input("\nGuess a letter: ").lower()
- 
 display=""
+guess_letter=input("\nGuess a letter: ").lower()
+    
 for letter in choosen_word:
     if guess_letter == letter:
         display+=letter
     else:
         display+="_"
-print(display)
+print(display) 
+
+while choosen_word != display:
+    guess_letter=input("\nGuess a letter: ").lower()
+    
+    for position,letter in enumerate(choosen_word):
+        if guess_letter == letter:
+            tempList=list(display)
+            tempList[position]=guess_letter
+            display="".join(tempList)
+    print(display)
+    
+if "_" not in display:
+    print("You've Won")
