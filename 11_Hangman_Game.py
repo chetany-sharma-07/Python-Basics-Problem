dc76b9d8cbf8eb688ae2c2a41d3f993a977441e0
chetany-sharma-07 <sharmachetany1004@gmail.com>
2026-02-02 20:10:49 +0530

    adding stages list and logic which shows different stages of hangman after each wrong guess.

--- 11_Hangman_Game.py
+++ 11_Hangman_Game.py
@@ -1,17 +1,79 @@
 import random
 
+# different stages of hangman lifes
+stages = [r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
 word_list=["aardvark", "baboon", "camel"]
 
+#   random word choosen from word_list
 choosen_word=random.choice(word_list)
-print(choosen_word)
+# print(choosen_word)
 
+# generating blank _ equal to the length of choosen word
 placeholder=""
 for position in range(len(choosen_word)):
     placeholder+="_"
 print(placeholder)
 
-correct_letter=[]
-game_over=False
+
+correct_letter=[] #use to store previously guessed correct letter of remembring later
+game_over=False #decide game over or not
+no_of_lifes=6 #total no of lifes
 
 while not game_over:
     display=""
@@ -26,6 +88,14 @@ while not game_over:
         else:
             display+="_"
     print(display) 
+
+    if guess_letter not in choosen_word:
+        print(stages[no_of_lifes])
+        no_of_lifes-=1
+        print(f"No of Lifes Remaining: {no_of_lifes+1}")
+        if no_of_lifes==-1:
+            game_over=True
+            print("You lose")
 
     if "_" not in display:
         game_over=True
