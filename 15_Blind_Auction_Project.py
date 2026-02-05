f08854a9eeff339e2f605080c621d3672071296a
chetany-sharma-07 <sharmachetany1004@gmail.com>
2026-02-05 21:07:13 +0530

    15_Blind_Auction_Project.py is done.

---
+++ 15_Blind_Auction_Project.py
@@ -0,0 +1,24 @@
+print("####################### WELCOME TO BLIND AUCTION PROGRAM ########################")
+
+bidder={}
+while True:
+    bidder_name=input("What is your name?: ")
+    bid=int(input("What's your bid?: $"))
+    bidder[bidder_name]=bid
+    any_other_bidder=input("Are there any other bidders? Type 'yes' or 'no'.\n").lower()
+    print("\n"*20)
+    if any_other_bidder=="no":
+        break
+
+print(bidder)
+
+# winner_name=""
+# winner_bid=0
+# for bidder_name in bidder:
+#     if bidder[bidder_name]>winner_bid:
+#         winner_name=bidder_name
+#         winner_bid=bidder[bidder_name]
+
+winner_name=max(bidder,key=bidder.get)
+winner_bid=bidder[winner_name]
+print(f"The winner is {winner_name} with a bid of ${winner_bid}.")
